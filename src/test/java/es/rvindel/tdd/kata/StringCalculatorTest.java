6ddf1ecd4b41639daa245a2a8de9c08743029c2b
Rafael Vindel <rvindel@autentia.com>
2016-02-04 20:29:01 +0100

    [REFACTOR_8] stubbing StringSplitter mock

--- src/test/java/es/rvindel/tdd/kata/StringCalculatorTest.java
+++ src/test/java/es/rvindel/tdd/kata/StringCalculatorTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class StringCalculatorTest {
 
@@ -20,26 +21,31 @@ public class StringCalculatorTest {
 
     @Test
     public void shouldReturnZeroWhenAddIsInvokedWithAnEmptyString() {
+        when(stringSplitter.split("")).thenReturn(new int[] {});
         assertThat(0, equalTo(sut.add("")));
     }
 
     @Test
     public void shouldReturnNumberWhenAddIsInvokedWithASingleNumberString() {
+        when(stringSplitter.split("5")).thenReturn(new int[] {5});
         assertThat(5, equalTo(sut.add("5")));
     }
 
     @Test
     public void shouldReturnSumWhenAddIsInvokedWithTwoNumbersSeparatedByComaString() {
+        when(stringSplitter.split("3,4")).thenReturn(new int[] {3, 4});
         assertThat(7, equalTo(sut.add("3,4")));
     }
 
     @Test
     public void shouldReturnSumWhenAddIsInvokedWithMoreThanTwoNumbersSeparatedByComaString() {
+        when(stringSplitter.split("3,4,5,6,7")).thenReturn(new int[] {3, 4, 5, 6, 7});
         assertThat(25, equalTo(sut.add("3,4,5,6,7")));
     }
 
     @Test
     public void shouldReturnSumWhenAddIsInvokedWithMoreThanTwoNumbersSeparatedByComaOrReturnCharacterString() {
+        when(stringSplitter.split("3,4\n5,6\n7")).thenReturn(new int[] {3, 4, 5, 6, 7});
         assertThat(25, equalTo(sut.add("3,4\n5,6\n7")));
     }
 }
