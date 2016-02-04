ae2873da751c78f7194fa2e02b5eacda0464f9bb
Rafael Vindel <rvindel@autentia.com>
2016-02-04 20:04:36 +0100

    [REFACTOR_6] change StringCalculator implementation with StringSplitter.split() new return type

--- src/main/java/es/rvindel/tdd/kata/StringCalculator.java
+++ src/main/java/es/rvindel/tdd/kata/StringCalculator.java
@@ -12,10 +12,9 @@ public class StringCalculator {
 
     public int add(String numbers) {
         if(!numbers.isEmpty()) {
-            final String[] numbersArray = stringSplitter.split(numbers);
-            return Arrays.asList(numbersArray)
-                    .stream()
-                    .mapToInt(number -> Integer.parseInt(number))
+            final int[] numbersArray = stringSplitter.split(numbers);
+            return Arrays
+                    .stream(numbersArray)
                     .sum();
         }
 
