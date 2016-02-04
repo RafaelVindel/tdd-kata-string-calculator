19936e93efc73ec48e365e0cfe607a239be1235e
Rafael Vindel <rvindel@autentia.com>
2016-02-04 22:27:37 +0100

    [REFACTOR] add private methods to remove duplicate code

--- src/main/java/es/rvindel/tdd/kata/CoreStringSplitter.java
+++ src/main/java/es/rvindel/tdd/kata/CoreStringSplitter.java
@@ -4,21 +4,33 @@ import java.util.Arrays;
 
 public class CoreStringSplitter implements StringSplitter {
 
+    private static final String CUSTOM_SEPARATOR_START_HEADING = "//";
+    private static final int CUSTOM_SEPARATOR_POSITION = 2;
+    private static final int CUSTOM_SEPARATOR_HEADING_LENGTH = 4;
+    private static final String DEFAULT_SEPARATORS = ",|\n";
+
     @Override
     public int[] split(String numbers) {
         if(!numbers.isEmpty()) {
-            if(numbers.startsWith("//")) {
-                final char customSeparator = numbers.charAt(2);
-                return Arrays.stream(numbers.substring(4).split(String.valueOf(customSeparator)))
-                        .mapToInt(number -> Integer.parseInt(number))
-                        .toArray();
-            } else {
-                return Arrays.stream(numbers.split(",|\n"))
-                        .mapToInt(number -> Integer.parseInt(number))
-                        .toArray();
-            }
+            return Arrays.stream(processNumbers(numbers).split(obtainSeparators(numbers)))
+                    .mapToInt(number -> Integer.parseInt(number))
+                    .toArray();
         }
 
         return new int[] {};
+    }
+
+    private String obtainSeparators(String numbers) {
+        if(numbers.startsWith(CUSTOM_SEPARATOR_START_HEADING)) {
+            return String.valueOf(numbers.charAt(CUSTOM_SEPARATOR_POSITION));
+        }
+        return DEFAULT_SEPARATORS;
+    }
+
+    private String processNumbers(String numbers) {
+        if(numbers.startsWith(CUSTOM_SEPARATOR_START_HEADING)) {
+            return numbers.substring(CUSTOM_SEPARATOR_HEADING_LENGTH);
+        }
+        return numbers;
     }
 }
