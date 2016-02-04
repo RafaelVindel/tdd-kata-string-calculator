4f4528bf6ebc0f4cade75d253fa4944c85d41be7
Rafael Vindel <rvindel@autentia.com>
2016-02-04 21:52:35 +0100

    [GREEN] test passes

--- src/main/java/es/rvindel/tdd/kata/CoreStringSplitter.java
+++ src/main/java/es/rvindel/tdd/kata/CoreStringSplitter.java
@@ -7,10 +7,18 @@ public class CoreStringSplitter implements StringSplitter {
     @Override
     public int[] split(String numbers) {
         if(!numbers.isEmpty()) {
-            return Arrays.stream(numbers.split(",|\n"))
-                    .mapToInt(number -> Integer.parseInt(number))
-                    .toArray();
+            if(numbers.startsWith("//")) {
+                final char customSeparator = numbers.charAt(2);
+                return Arrays.stream(numbers.substring(4).split(String.valueOf(customSeparator)))
+                        .mapToInt(number -> Integer.parseInt(number))
+                        .toArray();
+            } else {
+                return Arrays.stream(numbers.split(",|\n"))
+                        .mapToInt(number -> Integer.parseInt(number))
+                        .toArray();
+            }
         }
+
         return new int[] {};
     }
 }
